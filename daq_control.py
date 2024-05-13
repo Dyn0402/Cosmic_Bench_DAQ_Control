8f8c86ef9f9b6e3d3e34dd7ed049325bb9b6357f
Dylan Neff <Dyn0402@hotmail.com>
2024-05-13 15:21:38 +0200

    daq_control.py put condition of banco in with statement for banco and trigger
    Much cleaner if it works

--- daq_control.py
+++ daq_control.py
@@ -26,17 +26,17 @@ def main():
     banco_daq_ip, banco_daq_port = config.banco_info['ip'], config.banco_info['port']
     dedip196_ip, dedip196_port = config.processor_info['ip'], config.processor_info['port']
 
-    with (Client(hv_ip, hv_port) as hv_client,
-          # Client(trigger_switch_ip, trigger_switch_port) as trigger_switch_client,
-          Client(banco_daq_ip, banco_daq_port) as banco_daq_client,
+    with (Client(hv_ip, hv_port) as hv_client if True else None,
+          Client(trigger_switch_ip, trigger_switch_port) if banco else None as trigger_switch_client,
+          Client(banco_daq_ip, banco_daq_port) if banco else None as banco_daq_client,
           Client(dedip196_ip, dedip196_port) as processor_client):
         hv_client.send('Connected to daq_control')
         hv_client.receive()
         hv_client.send_json(config.hv_info)
 
         if banco:
-            # trigger_switch_client.send('Connected to daq_control')
-            # trigger_switch_client.receive()
+            trigger_switch_client.send('Connected to daq_control')
+            trigger_switch_client.receive()
 
             banco_daq_client.send('Connected to daq_control')
             banco_daq_client.receive()
@@ -56,9 +56,9 @@ def main():
             create_dir_if_not_exist(sub_top_out_dir)
             sub_out_dir = f'{sub_top_out_dir}{config.raw_daq_inner_dir}/'
             create_dir_if_not_exist(sub_out_dir)
-            # if banco:
-            #     trigger_switch_client.send('off')  # Turn off trigger to make sure daqs are synced
-            #     trigger_switch_client.receive()
+            if banco:
+                trigger_switch_client.send('off')  # Turn off trigger to make sure daqs are synced
+                trigger_switch_client.receive()
             # sub_run_name = sub_run['sub_run_name']
             # hv_client.send(f'Start {sub_run_name}')
             hv_client.send('Start')
@@ -72,10 +72,10 @@ def main():
 
                 daq_controller = DAQController(config.daq_config_path, sub_run['run_time'],
                                                sub_run_name, sub_run_dir, sub_out_dir)
-                # if banco:
-                #     daq_controller.run(trigger_switch_client)
-                # else:
-                daq_controller.run()
+                if banco:
+                    daq_controller.run(trigger_switch_client)
+                else:
+                    daq_controller.run()
 
                 if banco:
                     banco_daq_client.send('Stop')
