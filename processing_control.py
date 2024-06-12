59b4ae300094a5a020fb11370ed23fb0efebb86b
Dylan <Dyn0402@hotmail.com>
2024-06-12 21:07:12 +0200

    processing_control.py fixed feu num file num bug

--- processing_control.py
+++ processing_control.py
@@ -169,13 +169,13 @@ def filter_by_m3(out_dir, m3_tracking_dir, decoded_dir, detectors, det_info_dir,
             continue
         print(f'\n\nFiltering decoded files by M3 tracking in {m3_file}')
         run_name = get_run_name_from_fdf_file_name(m3_file)
-        feu_num = get_feu_num_from_fdf_file_name(m3_file)
+        file_num = get_file_num_from_fdf_file_name(m3_file)
         detector_geometries = get_detector_geometries(detectors, det_info_dir, included_detectors)
         traversing_event_ids = get_m3_det_traversing_events(f'{m3_tracking_dir}{m3_file}', detector_geometries)
         for det_file in os.listdir(decoded_dir):
             if not det_file.endswith('_array.root') or '_datrun_' not in det_file:
                 continue
-            if get_feu_num_from_fdf_file_name(det_file) != feu_num:
+            if get_file_num_from_fdf_file_name(det_file) != file_num:
                 continue
             if get_run_name_from_fdf_file_name(det_file) != run_name:
                 continue
