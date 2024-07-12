10f32df8b911afcff1f88bac159f0daf0d54775f
Dylan <Dyn0402@hotmail.com>
2024-07-12 12:41:18 +0200

    Added new run_config.py for new detector test

--- config/old_configs/run_config_banco_flipped1.py
+++ config/old_configs/run_config_banco_flipped1.py
@@ -13,7 +13,7 @@ import json
 
 class Config:
     def __init__(self):
-        self.run_name = 'new_strip_check_7-12-24'
+        self.run_name = 'banco_flipped_7-8-24'
         self.daq_dir = '/home/clas12/dylan/Run/'
         self.run_dir = f'{self.daq_dir}{self.run_name}/'
         self.data_out_dir = '/mnt/cosmic_data/Run/'
@@ -84,8 +84,8 @@ class Config:
 
         self.sub_runs = [
             {
-                'sub_run_name': 'new_detector_short',
-                'run_time': 1 * 60,  # Minutes
+                'sub_run_name': 'max_hv_long',
+                'run_time': (200 * 24) * 60,  # Minutes
                 'hvs': {
                     0: {
                         0: 600,
@@ -93,7 +93,6 @@ class Config:
                         2: 800,
                         3: 800,
                         6: 700,
-                        7: 700,
                         8: 500,
                         9: 500,
                         10: 500,
@@ -101,41 +100,6 @@ class Config:
                     },
                     2: {
                         0: 450,
-                        2: 460,
-                    },
-                    3: {
-                        1: 365,
-                        2: 410,
-                        3: 450,
-                        4: 450,
-                        5: 450,
-                        6: 450,
-                        8: 460,
-                        9: 460,
-                        10: 460,
-                        11: 460,
-                    }
-                }
-            },
-            {
-                'sub_run_name': 'new_detector_long',
-                'run_time': 4 * 60,  # Minutes
-                'hvs': {
-                    0: {
-                        0: 600,
-                        1: 600,
-                        2: 800,
-                        3: 800,
-                        6: 700,
-                        7: 700,
-                        8: 500,
-                        9: 500,
-                        10: 500,
-                        11: 500,
-                    },
-                    2: {
-                        0: 450,
-                        2: 460,
                     },
                     3: {
                         1: 365,
