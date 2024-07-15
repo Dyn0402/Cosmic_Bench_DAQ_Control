a6e114c712cb52ef860830bf1c0e431cf8f9b11c
Dylan Neff <Dyn0402@hotmail.com>
2024-07-15 10:14:47 +0200

    run_processor.py set to run sg1_hv_scan since I didn't have the sg1 det config file and processing failed

--- run_processor.py
+++ run_processor.py
@@ -14,8 +14,55 @@ from run_config import Config
 
 def main():
     # run_m3_tracking_hv7()
-    run_m3_filtering_max_hv_stats()
+    # run_m3_filtering_max_hv_stats()
+    run_filtering_cleanup_sg1_hv_scan()
     print('donzo')
+
+
+def run_filtering_cleanup_sg1_hv_scan():
+    config = Config()
+    drift_hvs = [700, 600]
+    resist_hvs = [460, 450, 440, 430, 420, 410, 400, 390]
+    dedip196_ip, dedip196_port = config.dedip196_processor_info['ip'], config.dedip196_processor_info['port']
+    sedip28_ip, sedip28_port = config.sedip28_processor_info['ip'], config.sedip28_processor_info['port']
+
+    dedip196_processor_client = Client(dedip196_ip, dedip196_port)
+    sedip28_processor_client = Client(sedip28_ip, sedip28_port)
+
+    with dedip196_processor_client as dedip196_processor, sedip28_processor_client as sedip28_processor:
+        dedip196_processor.send('Connected to run_processor')
+        dedip196_processor.receive()
+        dedip196_processor.send_json(config.dedip196_processor_info)
+        dedip196_processor.receive()
+        dedip196_processor.send_json({'included_detectors': config.included_detectors})
+        dedip196_processor.receive()
+        dedip196_processor.send_json({'detectors': config.detectors})
+        dedip196_processor.receive()
+
+        sedip28_processor.send('Connected to run_processor')
+        sedip28_processor.receive()
+        sedip28_processor.send_json(config.sedip28_processor_info)
+
+        for drift_hv in drift_hvs:
+            for resist_hv in resist_hvs:
+                sub_run_name = f'drift_{drift_hv}_resist_{resist_hv}'
+                dedip196_processor.send(f'Decode FDFs {sub_run_name}', silent=False)
+                dedip196_processor.receive(silent=False)
+                sedip28_processor.send(f'Run M3 Tracking {sub_run_name}', silent=False)
+                sedip28_processor.receive(silent=False)
+                sedip28_processor.receive(silent=False)  # Wait for tracking to finish
+                dedip196_processor.receive(silent=False)  # Wait for decoding to finish
+                # Run filtering
+                dedip196_processor.send(f'Filter By M3 {sub_run_name}', silent=False)
+                dedip196_processor.receive(silent=False)
+                dedip196_processor.receive(silent=False)  # Wait for filtering to finish
+                # Remove all but filtered files
+                # dedip196_processor.send(f'Clean Up Unfiltered {sub_run_name}', silent=False)
+                # dedip196_processor.receive(silent=False)
+                # dedip196_processor.receive(silent=False)  # Wait for cleanup to finish
+
+        dedip196_processor.send('Finished')
+        sedip28_processor.send('Finished')
 
 
 def run_m3_filtering_max_hv_stats():
