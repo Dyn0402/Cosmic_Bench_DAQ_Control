ec89fa9cdd0b26432e9f136a8e70c5a92d03db86
Dylan <Dyn0402@hotmail.com>
2024-07-19 14:43:45 +0200

    trigger_manual.py added sleep time

--- trigger_manual.py
+++ trigger_manual.py
@@ -9,6 +9,7 @@ Created as Cosmic_Bench_DAQ_Control/trigger_manual
 """
 
 import sys
+from time import sleep
 from Client import Client
 
 
@@ -18,12 +19,17 @@ def main():
         print('No command given')
         return
     state = argv[1]
+    if len(argv) > 2:
+        sleep_time = argv[2]
+    else:
+        sleep_time = 5
     trigger_switch_ip, trigger_switch_port = '192.168.10.101', 1100
     with Client(trigger_switch_ip, trigger_switch_port) as trigger_switch_client:
         trigger_switch_client.send('Connected to trigger manual control')
         trigger_switch_client.receive()
         trigger_switch_client.send(state)
         trigger_switch_client.receive()
+        sleep(sleep_time)
         trigger_switch_client.send('Finished')
         trigger_switch_client.receive()
     print('donzo')
