449fd0010da15975264c1bc79e6fbb0a6e1c8eca
Dylan Neff <Dyn0402@hotmail.com>
2025-02-04 12:00:56 +0100

    run_config.py set to run 2-4 ss1 test with updated position

--- run_config.py
+++ run_config.py
@@ -13,7 +13,7 @@ import json
 
 class Config:
     def __init__(self):
-        self.run_name = 'ss1_test_1-31-25'
+        self.run_name = 'ss1_test_2-4-25'
         self.daq_dir = '/home/clas12/dylan/Run/'
         self.run_dir = f'{self.daq_dir}{self.run_name}/'
         self.data_out_dir = '/mnt/cosmic_data/Run/'
