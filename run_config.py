0f5422f29e4f5f37332d31246eed0f2c1dfd58c8
Dylan <Dyn0402@hotmail.com>
2025-09-26 19:18:08 +0200

    run_config.py fixed another dream path

--- run_config.py
+++ run_config.py
@@ -15,7 +15,8 @@ import copy
 class Config:
     def __init__(self):
         self.run_name = 'rd542_strip_1_beam_test_test_9-26-25'
-        self.daq_dir = '/home/clas12/dylan/Run/'  # Maybe kill
+        # self.daq_dir = '/home/clas12/dylan/Run/'  # Maybe kill
+        self.daq_dir = '/local/home/banco/dylan/Run/'  # Maybe kill
         self.run_dir = f'{self.daq_dir}{self.run_name}/'  # Maybe kill
         self.data_out_dir = '/mnt/cosmic_data/Run/'
         self.run_out_dir = f'{self.data_out_dir}{self.run_name}/'
