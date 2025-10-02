0ac30710961fa46a33c567c6fd6080a331df9859
Dylan <Dyn0402@hotmail.com>
2025-10-02 13:34:03 +0200

    Updated start_trigger_control.sh again

--- start_trigger_control.sh
+++ start_trigger_control.sh
@@ -6,10 +6,10 @@ REMOTE_DIR="dylan/Cosmic_Bench_DAQ_Control"
 SCRIPT="trigger_switch_control.py"
 SCREEN_NAME="trigger_control"
 
-ssh -t "$PI_USER@$PI_HOST" << EOF
-cd "$REMOTE_DIR"
-# Start a new screen session if it doesn't exist, or attach
-screen -S "$SCREEN_NAME" -X quit 2>/dev/null  # optional: kill existing session
-screen -dmS "$SCREEN_NAME" bash -c "python3 '$SCRIPT'; exec bash"
-screen -r "$SCREEN_NAME"
-EOF
+ssh -t "$PI_USER@$PI_HOST" "
+cd '$REMOTE_DIR'
+# Start screen session (detached) and run Python
+screen -dmS '$SCREEN_NAME' bash -c 'python3 $SCRIPT; exec bash'
+# Attach to the screen to see output
+screen -r '$SCREEN_NAME'
+"
