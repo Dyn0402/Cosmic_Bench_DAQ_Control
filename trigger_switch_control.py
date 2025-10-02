79e323495ac44b3dc45c7365a7539743e9c14ba1
Dylan <Dyn0402@hotmail.com>
2025-10-02 14:20:35 +0200

    trigger_switch_control.py added send_triggers functionality
    To test

--- trigger_switch_control.py
+++ trigger_switch_control.py
@@ -8,8 +8,9 @@ Created as Cosmic_Bench_DAQ_Control/trigger_switch_control.py
 @author: Dylan Neff, Dylan
 """
 
-from Server import Server
+import time
 import RPi.GPIO as GPIO
+from Server import Server
 
 
 def main():
@@ -28,6 +29,18 @@ def main():
                     elif 'off' in res:
                         trigger_switch('off', pin=gpio_pin)
                         server.send(f'Trigger switch turned off')
+                    elif 'send triggers' in res:
+                        try:
+                            parts = res.strip().split()
+                            num_triggers = int(parts[2])
+                            freq_hz = float(parts[3])
+                            pulse_freq_ratio = float(parts[4])
+                            pulse_level = parts[5]
+                            send_triggers(num_triggers=num_triggers, freq_hz=freq_hz,
+                                          pulse_freq_ratio=pulse_freq_ratio, pulse_level=pulse_level, pin=gpio_pin)
+                            server.send(f'Sent {num_triggers} triggers at {freq_hz} Hz with pulse ratio {pulse_freq_ratio} and level {pulse_level}')
+                        except (IndexError, ValueError) as e:
+                            server.send(f'Error parsing send triggers command: {e}')
                     else:
                         server.send('Unknown Command')
                     res = server.receive()
@@ -57,6 +70,35 @@ def trigger_switch(state, pin=17):
         GPIO.output(pin, GPIO.LOW)
     else:
         print('Unknown trigger switch state')
+
+
+def send_triggers(num_triggers=100, freq_hz=10, pulse_freq_ratio=1, pulse_level='low', pin=17):
+    """
+    Send a number of triggers at a given frequency
+    :param num_triggers: Number of triggers to send
+    :param freq_hz: Frequency in Hz to send triggers
+    :param pulse_freq_ratio: Ratio of pulse width to frequency period (0 < ratio <= 1)
+    :param pulse_level: Level of the pulse, either 'low' or 'high'
+    :param pin: GPIO pin number to use
+    :return:
+    """
+    period = 1.0 / freq_hz
+    pulse_length = period * pulse_freq_ratio / (pulse_freq_ratio + 1)
+    delay = period - pulse_length
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(pin, GPIO.OUT)
+    for _ in range(num_triggers):
+        if pulse_level == 'low':
+            GPIO.output(pin, GPIO.LOW)
+        else:
+            GPIO.output(pin, GPIO.HIGH)
+        time.sleep(pulse_length)
+        if pulse_level == 'low':
+            GPIO.output(pin, GPIO.HIGH)
+        else:
+            GPIO.output(pin, GPIO.LOW)
+        time.sleep(delay)
+    GPIO.cleanup()
 
 
 if __name__ == '__main__':
