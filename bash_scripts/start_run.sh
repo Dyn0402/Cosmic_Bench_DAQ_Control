f629f8ddb31968247f99e4640d995a0dd9c8ed0e
Dylan <Dyn0402@hotmail.com>
2025-10-07 12:27:05 +0200

    start_run.sh don't detach

--- bash_scripts/start_run.sh
+++ bash_scripts/start_run.sh
@@ -2,8 +2,5 @@
 SESSION="daq_control"
 COMMAND="python daq_control_new.py"
 
-# Send the command to the session and execute it in background (&)
+# Send the command to the session and execute it
 tmux send-keys -t "$SESSION" "$COMMAND" C-m
-
-# (Optional) detach from the session (no effect if you weren't attached)
-tmux detach -s "$SESSION"
