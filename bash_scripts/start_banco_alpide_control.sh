8f985bbfad5124a782a88d8277e68ef968c42d4c
Dylan Neff <Dyn0402@hotmail.com>
2025-10-10 13:52:25 +0200

    start_banco_alpide_control.sh try sourcing .bashrc first

--- bash_scripts/start_banco_alpide_control.sh
+++ bash_scripts/start_banco_alpide_control.sh
@@ -10,7 +10,7 @@ ssh -t "$USER@$HOST" "
 cd '$REMOTE_DIR'
 # Start screen session (detached) and run Python
 screen -S "$SCREEN_NAME" -X quit 2>/dev/null  # optional: kill existing session
-screen -dmS '$SCREEN_NAME' bash -c 'setup-o2-root && python3 $SCRIPT; exec bash'
+screen -dmS '$SCREEN_NAME' bash -c 'source ~/.bashrc && setup-o2-root && python3 $SCRIPT; exec bash'
 # Attach to the screen to see output
 screen -r '$SCREEN_NAME'
 "
