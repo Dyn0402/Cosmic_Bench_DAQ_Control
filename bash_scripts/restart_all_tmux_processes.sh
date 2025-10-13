fd7b7ee9fc2d041003a5ce16563e464739fa0d30
Dylan <Dyn0402@hotmail.com>
2025-10-13 18:40:07 +0200

    restart_all_tmux_processes.sh try attaching to a screen

--- bash_scripts/restart_all_tmux_processes.sh
+++ bash_scripts/restart_all_tmux_processes.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-# Start the restart in background with a small delay
-nohup bash -c "sleep 2 && /local/home/banco/dylan/Cosmic_Bench_DAQ_Control/start_all_servers.sh" >/dev/null 2>&1 &
+# Start a detached screen that will handle the restart
+screen -dmS restart_tmux bash -c '
+  sleep 2
+  /local/home/banco/dylan/Cosmic_Bench_DAQ_Control/start_all_servers.sh
+'
 
 # Kill tmux server
 tmux kill-server
