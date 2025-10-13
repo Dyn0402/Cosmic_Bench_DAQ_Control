64b15136e60de542fc1472b7a385bcbb0cd1ef0b
Dylan <Dyn0402@hotmail.com>
2025-10-13 18:24:51 +0200

    restart_all_tmux_processes.sh try nohup

--- bash_scripts/restart_all_tmux_processes.sh
+++ bash_scripts/restart_all_tmux_processes.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# Schedule restart after tmux exits
-echo "/local/home/banco/dylan/Cosmic_Bench_DAQ_Control/start_servers.sh" | at now + 2 seconds
+# Start the restart in background with a small delay
+nohup bash -c "sleep 2 && /local/home/banco/dylan/Cosmic_Bench_DAQ_Control/start_all_servers.sh" >/dev/null 2>&1 &
 
 # Kill tmux server
 tmux kill-server
