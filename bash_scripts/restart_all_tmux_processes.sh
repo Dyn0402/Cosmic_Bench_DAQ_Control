bab89a4823269db0c629a22b0868d50c3ba3f8f5
Dylan <Dyn0402@hotmail.com>
2025-10-13 18:20:55 +0200

    restart_all_tmux_processes.sh fixed path

--- bash_scripts/restart_all_tmux_processes.sh
+++ bash_scripts/restart_all_tmux_processes.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Schedule restart after tmux exits
-echo "/local/home/banco/dylan/Cosmic_Bench_DAQ_Control/start_all_servers.sh" | at now + 2 seconds
+echo "/local/home/banco/dylan/Cosmic_Bench_DAQ_Control/start_servers.sh" | at now + 2 seconds
 
 # Kill tmux server
 tmux kill-server
