a936291cad4ca9f3a949d12722a79b04b3fcbb09
Dylan <Dyn0402@hotmail.com>
2025-10-15 16:52:20 +0200

    Setting to run fe test again with flipped connectors

--- run_config.py
+++ run_config.py
@@ -15,7 +15,7 @@ import copy
 
 class Config:
     def __init__(self):
-        self.run_name = 'rd542_plein_vfp_1_fe_test_10-15-25'
+        self.run_name = 'rd542_plein_vfp_1_fe_test2_10-15-25'
         self.daq_dir = '/local/home/banco/dylan/Run/'  # Maybe kill
         self.run_dir = f'{self.daq_dir}{self.run_name}/'  # Maybe kill
         # self.base_out_dir = '/mnt_cosmic_data/'
