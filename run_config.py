68bae4e59b9686d00ad188d2c254bf0c314c24f5
Dylan <Dyn0402@hotmail.com>
2025-10-21 12:19:58 +0200

    run_config.py try running in batch mode

--- run_config.py
+++ run_config.py
@@ -45,7 +45,7 @@ class Config:
             'go_timeout': 5 * 60,  # Seconds to wait for 'Go' response from RunCtrl before assuming failure
             'max_run_time_addition': 60 * 5,  # Seconds to add to requested run time before killing run
             'copy_on_fly': True,  # True to copy raw data to out dir during run, False to copy after run
-            'batch_mode': False  # Run Dream RunCtrl in batch mode. Not implemented for cosmic bench C
+            'batch_mode': True  # Run Dream RunCtrl in batch mode. Not implemented for cosmic bench C
         }
 
         self.banco_info = {
