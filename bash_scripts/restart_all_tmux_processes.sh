68a1d030c1a91fbce18a2448e2f28944594577e5
Dylan <Dyn0402@hotmail.com>
2025-11-08 03:34:59 +0100

    restart_all_tmux_processes.sh think I have more reliable implementation

--- bash_scripts/restart_all_tmux_processes.sh
+++ bash_scripts/restart_all_tmux_processes.sh
@@ -1,31 +1,33 @@
 #!/bin/bash
 
-# Patterns of tmux session names to KEEP
-KEEP_PATTERNS=("decoder_" "processor_" "analyzer_")
+tmux list-sessions -F "#{session_name}" 2>/dev/null | grep -vE '^(decoder_|processor_|analyzer_)' | xargs -r -n1 tmux kill-session -t
 
-# Get all tmux sessions
-sessions=$(tmux list-sessions -F "#{session_name}" 2>/dev/null)
-
-# Restart servers in detached screen
-screen -dmS restart_tmux bash -c '
-  sleep 20
-  /local/home/banco/dylan/Cosmic_Bench_DAQ_Control/start_servers.sh
-'
-
-# Determine which to kill
-for s in $sessions; do
-  keep=false
-  for pat in "${KEEP_PATTERNS[@]}"; do
-    if [[ $s == $pat* ]]; then
-      keep=true
-      break
-    fi
-  done
-  if ! $keep; then
-    echo "Killing tmux session: $s"
-    tmux kill-session -t "$s"
-  fi
-done
+## Patterns of tmux session names to KEEP
+#KEEP_PATTERNS=("decoder_" "processor_" "analyzer_")
+#
+## Get all tmux sessions
+#sessions=$(tmux list-sessions -F "#{session_name}" 2>/dev/null)
+#
+## Restart servers in detached screen
+#screen -dmS restart_tmux bash -c '
+#  sleep 20
+#  /local/home/banco/dylan/Cosmic_Bench_DAQ_Control/start_servers.sh
+#'
+#
+## Determine which to kill
+#for s in $sessions; do
+#  keep=false
+#  for pat in "${KEEP_PATTERNS[@]}"; do
+#    if [[ $s == $pat* ]]; then
+#      keep=true
+#      break
+#    fi
+#  done
+#  if ! $keep; then
+#    echo "Killing tmux session: $s"
+#    tmux kill-session -t "$s"
+#  fi
+#done
 
 
 ## Start a detached screen that will handle the restart
