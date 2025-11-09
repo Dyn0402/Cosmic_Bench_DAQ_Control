63a7ef7bb3227b332caafc335e8a946036b3cbb1
Dylan <Dyn0402@hotmail.com>
2025-11-09 11:43:05 +0100

    Added dream_desync_event_id_check.py to check Irakli's hypothesis that dream desync is ok

---
+++ quick_scripts/dream_desync_event_id_check.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on November 09 11:38 2025
+Created in PyCharm
+Created as Cosmic_Bench_DAQ_Control/dream_desync_event_id_check
+
+@author: Dylan Neff, dn277127
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import uproot
+
+
+def main():
+    base_dir = '/mnt/data/beam_sps_25/Bad_Runs_Repo/run_21/resist_hv_-10/'
+    banco_root_path = f'{base_dir}/banco_data/multinoiseScan_251109_110345-B0-ladder157.root'
+    dream_root_path = f'{base_dir}/filtered_root/TbSPS25_resist_hv_-10_datrun_251109_11H03_000_05_decoded_array_filtered.root'
+
+    banco_file = uproot.open(banco_root_path)
+    dream_file = uproot.open(dream_root_path)
+
+    print('banco keys:', banco_file.keys())
+    print('dream keys:', dream_file.keys())
+
+    print('donzo')
+
+
+if __name__ == '__main__':
+    main()
