521c4be131a9d080c2bbe0d3404d00810d0c103c
Dylan <Dyn0402@hotmail.com>
2025-11-11 14:56:38 +0100

    run_config_beam.py set to run_48

--- run_config_beam.py
+++ run_config_beam.py
@@ -15,7 +15,7 @@ import copy
 
 class Config:
     def __init__(self, config_path=None):
-        self.run_name = 'run_47'
+        self.run_name = 'run_48'
         self.base_out_dir = '/mnt/data/beam_sps_25/'
         self.data_out_dir = f'{self.base_out_dir}Run/'
         self.run_out_dir = f'{self.data_out_dir}{self.run_name}/'
@@ -133,7 +133,7 @@ class Config:
         self.sub_runs = [
             {
                 'sub_run_name': 'resist_mesh_scan_-0',
-                'run_time': 5,  # Minutes
+                'run_time': 8,  # Minutes
                 'hvs': {
                     '2': {
                         '0': 830,
@@ -191,25 +191,25 @@ class Config:
         # self.sub_runs = self.sub_runs[1:]
 
         # Append copies of sub_runs where drifts are decreased by 50V for each sub_run
-        template = self.sub_runs[0]
-        resist_diffs = [-5, -10, -15, -20, -25, -30, -35, -40, -45, -50]
-        for resist_diff in resist_diffs:
-            sub_run = copy.deepcopy(template)
-            sub_run['sub_run_name'] = f'resist_mesh_hv_{resist_diff}'
-
-            card = '2'
-            channels = ['0', '1', '2', '3', '4', '7', '8', '9', '10']  # Resist channels
-            # channels = ['3']  # Resist channels
-            for channel in sub_run['hvs'][card]:
-                if channel in channels:
-                    sub_run['hvs'][card][channel] = sub_run['hvs'][card][channel] + resist_diff
-
-            card = '5'
-            channels = ['2', '3', '6', '7', '8', '9', '10', '11']  # Drift + resist channels
-            for channel in sub_run['hvs'][card]:
-                if channel in channels:
-                    sub_run['hvs'][card][channel] = sub_run['hvs'][card][channel] + resist_diff
-            self.sub_runs.append(sub_run)
+        # template = self.sub_runs[0]
+        # resist_diffs = [-5, -10, -15, -20, -25, -30, -35, -40, -45, -50]
+        # for resist_diff in resist_diffs:
+        #     sub_run = copy.deepcopy(template)
+        #     sub_run['sub_run_name'] = f'resist_mesh_hv_{resist_diff}'
+        #
+        #     card = '2'
+        #     channels = ['0', '1', '2', '3', '4', '7', '8', '9', '10']  # Resist channels
+        #     # channels = ['3']  # Resist channels
+        #     for channel in sub_run['hvs'][card]:
+        #         if channel in channels:
+        #             sub_run['hvs'][card][channel] = sub_run['hvs'][card][channel] + resist_diff
+        #
+        #     card = '5'
+        #     channels = ['2', '3', '6', '7', '8', '9', '10', '11']  # Drift + resist channels
+        #     for channel in sub_run['hvs'][card]:
+        #         if channel in channels:
+        #             sub_run['hvs'][card][channel] = sub_run['hvs'][card][channel] + resist_diff
+        #     self.sub_runs.append(sub_run)
 
         # drift_diffs_eic = [-50, -100, -150, -200, -250, -300, -350, -400, -450]
         # drift_diffs_p2 = [-20, -40, -60, -80, -100, -120, -140, -160, -180]
@@ -232,12 +232,12 @@ class Config:
         #     self.sub_runs.append(sub_run)
 
         # Append copies of sub_runs with same voltages but different run names
-        # template = self.sub_runs[0]
-        # for i in range(1, 5):
-        #     sub_run = copy.deepcopy(template)
-        #     sub_run['sub_run_name'] = f'rotation_45_test_{i}'
-        #     sub_run['run_time'] = 10  # Minutes
-        #     self.sub_runs.append(sub_run)
+        template = self.sub_runs[0]
+        for i in range(1, 5):
+            sub_run = copy.deepcopy(template)
+            sub_run['sub_run_name'] = f'rotation_45_test_{i}'
+            sub_run['run_time'] = 10  # Minutes
+            self.sub_runs.append(sub_run)
 
         self.bench_geometry = {
             'board_thickness': 5,  # mm  Thickness of PCB for test boards  Guess!
@@ -246,7 +246,7 @@ class Config:
             'banco_arm_separation_z': 172 - 41,  # mm from bottom of lower banco arm to bottom of upper banco arm
             'banco_arm_right_y': 34 + 100,  # mm from center of banco to right edge of banco arm
             'banco_arm_length_y': 230,  # mm from left edge of banco arm to right edge of banco arm
-            'banco_moveable_y_position': 1000.0,  # mm  Offset from moving table. Positive moves banco up.
+            'banco_moveable_y_position': 0.0,  # mm  Offset from moving table. Positive moves banco up.
         }
 
         self.included_detectors = ['banco_ladder160', 'banco_ladder163', 'banco_ladder157', 'banco_ladder162',
