7833043137947ce1e635a5a761262f7a505e64f5
Dylan <Dyn0402@hotmail.com>
2025-11-11 22:08:39 +0100

    Added cheap_efficiency_plot.py

---
+++ quick_scripts/cheap_efficiency_plot.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on November 11 21:55 2025
+Created in PyCharm
+Created as Cosmic_Bench_DAQ_Control/cheap_efficiency_plot
+
+@author: Dylan Neff, dn277127
+"""
+
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+def main():
+    base_dir = '/mnt/data/beam_sps_25/Analysis/'
+    run_name = 'run_47'
+    detector_names = ['rd5_plein_saral_2']
+    sub_runs = {
+        'resist_mesh_scan_-0': 0,
+        'resist_mesh_hv_-10': -10,
+        'resist_mesh_hv_-15': -15,
+        'resist_mesh_hv_-20': -20,
+        'resist_mesh_hv_-25': -25,
+        'resist_mesh_hv_-30': -30,
+    }
+    for detector_name in detector_names:
+        efficiencies = []
+        total_events = []
+        voltages = []
+        for sub_run_name, voltage in sub_runs.items():
+            analysis_dir = f'{base_dir}{run_name}/{sub_run_name}/'
+            efficiency_file = f'{analysis_dir}/{detector_name}/coincident_events.csv'
+            if not os.path.exists(efficiency_file):
+                print(f'Efficiency file not found: {efficiency_file}')
+                continue
+            df = pd.read_csv(efficiency_file)
+            efficiency = df['fraction_of_total_events'].iloc[0]
+            total_event = df['total_triggers'].iloc[0]
+            efficiencies.append(efficiency)
+            total_events.append(total_event)
+            voltages.append(voltage)
+        fig, ax = plt.subplots(figsize=(8, 6))
+        ax.plot(voltages, efficiencies, marker='o', linestyle='-')
+        ax.set_xlabel('Mesh Voltage (V)')
+        ax.set_ylabel('Efficiency')
+        ax.set_title(f'Efficiency vs Mesh Voltage for {detector_name} in {run_name}')
+        ax.grid(True)
+        plt.tight_layout()
+
+        fig, ax = plt.subplots(figsize=(8, 6))
+        ax.plot(voltages, total_events, marker='o', linestyle='-')
+        ax.set_xlabel('Mesh Voltage (V)')
+        ax.set_ylabel('Total Events')
+    plt.show()
+    print('donzo')
+
+
+if __name__ == '__main__':
+    main()
