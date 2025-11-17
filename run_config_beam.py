7a1d6176810ce5be1880e1a4ab92943127d684fe
Dylan <Dyn0402@hotmail.com>
2025-11-17 05:42:07 +0100

    run_config_beam.py set for banco scan at -15 degrees

--- run_config_beam.py
+++ run_config_beam.py
@@ -15,7 +15,7 @@ import copy
 
 class Config:
     def __init__(self, config_path=None):
-        self.run_name = 'run_103'
+        self.run_name = 'run_120'
         self.base_out_dir = '/mnt/data/beam_sps_25/'
         self.data_out_dir = f'{self.base_out_dir}Run/'
         self.run_out_dir = f'{self.data_out_dir}{self.run_name}/'
@@ -137,7 +137,7 @@ class Config:
         hv_adjust = 0
         self.sub_runs = [
             {
-                'sub_run_name': f'rotation_0_drift_scan_0',
+                'sub_run_name': f'rotation_-15_banco_scan_0',
                 'run_time': 10,  # Minutes
                 'hvs': {
                     '2': {
@@ -225,45 +225,45 @@ class Config:
         #     #         sub_run['hvs'][card][channel] = sub_run['hvs'][card][channel] + resist_diff
         #     self.sub_runs.append(sub_run)
         #
-        template = self.sub_runs[0]
-        drift_diffs_eic = [-375, -350, -325, -300, -275, -250, -225, -200, -175, -150, -125,
-                           -100, -75, -50, -25, -400]
-        # drift_diffs_eic = [-50, -100, -150, -200, -250, -300, -350, -400, -450]
-        # drift_diffs_p2 = [-20, -40, -60, -80, -100, -120, -140, -160, -180]
-        # for drift_diff_eic, drift_diff_p2 in zip(drift_diffs_eic, drift_diffs_p2):
-        for drift_diff_eic in drift_diffs_eic:
-            sub_run = copy.deepcopy(template)
-            sub_run['sub_run_name'] = f'rotation_0_drift_scan_{drift_diff_eic}'
-
-            card = '5'
-            channels = ['0', '1', '4', '5']  # Drift channels
-            for channel in sub_run['hvs'][card]:
-                if channel in channels:
-                    sub_run['hvs'][card][channel] = sub_run['hvs'][card][channel] + drift_diff_eic
-
-            # card = '5'
-            # channels = ['6', '8', '10']
-            # for channel in sub_run['hvs'][card]:
-            #     if channel in channels:
-            #         sub_run['hvs'][card][channel] = sub_run['hvs'][card][channel] + drift_diff_p2
-
-            self.sub_runs.append(sub_run)
-
-        # Append to start of sub_runs a duplicate of the last sub_run with 0 minute run time
-        template = copy.deepcopy(self.sub_runs[0])
-        template['sub_run_name'] = 'long_wait_for_beam'
-        template['run_time'] = 100  # Minutes
-        self.sub_runs.insert(0, template)
+        # template = self.sub_runs[0]
+        # drift_diffs_eic = [-375, -350, -325, -300, -275, -250, -225, -200, -175, -150, -125,
+        #                    -100, -75, -50, -25, -400]
+        # # drift_diffs_eic = [-50, -100, -150, -200, -250, -300, -350, -400, -450]
+        # # drift_diffs_p2 = [-20, -40, -60, -80, -100, -120, -140, -160, -180]
+        # # for drift_diff_eic, drift_diff_p2 in zip(drift_diffs_eic, drift_diffs_p2):
+        # for drift_diff_eic in drift_diffs_eic:
+        #     sub_run = copy.deepcopy(template)
+        #     sub_run['sub_run_name'] = f'rotation_0_drift_scan_{drift_diff_eic}'
+        #
+        #     card = '5'
+        #     channels = ['0', '1', '4', '5']  # Drift channels
+        #     for channel in sub_run['hvs'][card]:
+        #         if channel in channels:
+        #             sub_run['hvs'][card][channel] = sub_run['hvs'][card][channel] + drift_diff_eic
+        #
+        #     # card = '5'
+        #     # channels = ['6', '8', '10']
+        #     # for channel in sub_run['hvs'][card]:
+        #     #     if channel in channels:
+        #     #         sub_run['hvs'][card][channel] = sub_run['hvs'][card][channel] + drift_diff_p2
+        #
+        #     self.sub_runs.append(sub_run)
+        #
+        # # Append to start of sub_runs a duplicate of the last sub_run with 0 minute run time
+        # template = copy.deepcopy(self.sub_runs[0])
+        # template['sub_run_name'] = 'long_wait_for_beam'
+        # template['run_time'] = 100  # Minutes
+        # self.sub_runs.insert(0, template)
 
         # Append copies of sub_runs with same voltages but different run names
-        # template = self.sub_runs[0]
-        # for i in range(1, 8):
-        #     sub_run = copy.deepcopy(template)
-        #     # Get sub_run name and just strip off everything after last underscore
-        #     sub_run_name_base = sub_run['sub_run_name'].rsplit('_', 1)[0]
-        #     sub_run['sub_run_name'] = f'{sub_run_name_base}_{i}'
-        #     # sub_run['run_time'] = 10  # Minutes
-        #     self.sub_runs.append(sub_run)
+        template = self.sub_runs[0]
+        for i in range(1, 8):
+            sub_run = copy.deepcopy(template)
+            # Get sub_run name and just strip off everything after last underscore
+            sub_run_name_base = sub_run['sub_run_name'].rsplit('_', 1)[0]
+            sub_run['sub_run_name'] = f'{sub_run_name_base}_{i}'
+            # sub_run['run_time'] = 10  # Minutes
+            self.sub_runs.append(sub_run)
 
         self.bench_geometry = {
             'board_thickness': 5,  # mm  Thickness of PCB for test boards  Guess!
@@ -272,7 +272,7 @@ class Config:
             'banco_arm_separation_z': 172 - 41,  # mm from bottom of lower banco arm to bottom of upper banco arm
             'banco_arm_right_y': 34 + 100,  # mm from center of banco to right edge of banco arm
             'banco_arm_length_y': 230,  # mm from left edge of banco arm to right edge of banco arm
-            'banco_moveable_y_position': 450.0,  # mm  Offset from moving table. Positive moves banco up.
+            'banco_moveable_y_position': 0.0,  # mm  Offset from moving table. Positive moves banco up.
         }
 
         # self.included_detectors = ['banco_ladder160', 'banco_ladder163', 'banco_ladder157', 'banco_ladder162',
@@ -359,7 +359,7 @@ class Config:
                 },
                 'det_orientation': {
                     'x': 0,  # deg  Rotation about x axis
-                    'y': 0,  # deg  Rotation about y axis
+                    'y': -15,  # deg  Rotation about y axis
                     'z': 0,  # deg  Rotation about z axis
                 },
                 'hv_channels': {
@@ -390,7 +390,7 @@ class Config:
                 },
                 'det_orientation': {
                     'x': 0,  # deg  Rotation about x axis
-                    'y': 0,  # deg  Rotation about y axis
+                    'y': -15,  # deg  Rotation about y axis
                     'z': 0,  # deg  Rotation about z axis
                 },
                 'hv_channels': {
@@ -421,7 +421,7 @@ class Config:
                 },
                 'det_orientation': {
                     'x': 0,  # deg  Rotation about x axis
-                    'y': 0,  # deg  Rotation about y axis
+                    'y': -15,  # deg  Rotation about y axis
                     'z': 0,  # deg  Rotation about z axis
                 },
                 'hv_channels': {
@@ -452,7 +452,7 @@ class Config:
                 },
                 'det_orientation': {
                     'x': 0,  # deg  Rotation about x axis
-                    'y': 0,  # deg  Rotation about y axis
+                    'y': -15,  # deg  Rotation about y axis
                     'z': 0,  # deg  Rotation about z axis
                 },
                 'hv_channels': {
@@ -484,7 +484,7 @@ class Config:
                 },
                 'det_orientation': {
                     'x': 0,  # deg  Rotation about x axis
-                    'y': 0,  # deg  Rotation about y axis
+                    'y': -15,  # deg  Rotation about y axis
                     'z': 0,  # deg  Rotation about z axis
                 },
                 'hv_channels': {
@@ -515,7 +515,7 @@ class Config:
                 },
                 'det_orientation': {
                     'x': 0,  # deg  Rotation about x axis
-                    'y': 0,  # deg  Rotation about y axis
+                    'y': -15,  # deg  Rotation about y axis
                     'z': 0,  # deg  Rotation about z axis
                 },
                 'hv_channels': {
@@ -547,7 +547,7 @@ class Config:
                 },
                 'det_orientation': {
                     'x': 0,  # deg  Rotation about x axis
-                    'y': 0,  # deg  Rotation about y axis
+                    'y': -15,  # deg  Rotation about y axis
                     'z': 0,  # deg  Rotation about z axis
                 },
                 'hv_channels': {
@@ -579,7 +579,7 @@ class Config:
                 },
                 'det_orientation': {
                     'x': 0,  # deg  Rotation about x axis
-                    'y': 0,  # deg  Rotation about y axis
+                    'y': -15,  # deg  Rotation about y axis
                     'z': 0,  # deg  Rotation about z axis
                 },
                 'hv_channels': {
@@ -610,7 +610,7 @@ class Config:
                 },
                 'det_orientation': {
                     'x': 0,  # deg  Rotation about x axis
-                    'y': 0,  # deg  Rotation about y axis
+                    'y': -15,  # deg  Rotation about y axis
                     'z': 0,  # deg  Rotation about z axis
                 },
                 'hv_channels': {
@@ -641,7 +641,7 @@ class Config:
                 },
                 'det_orientation': {
                     'x': 0,  # deg  Rotation about x axis
-                    'y': 0,  # deg  Rotation about y axis
+                    'y': -15,  # deg  Rotation about y axis
                     'z': 0,  # deg  Rotation about z axis
                 },
                 'hv_channels': {
@@ -674,7 +674,7 @@ class Config:
                 },
                 'det_orientation': {
                     'x': 0,  # deg  Rotation about x axis
-                    'y': 0,  # deg  Rotation about y axis
+                    'y': -15,  # deg  Rotation about y axis
                     'z': 0,  # deg  Rotation about z axis
                 },
                 'hv_channels': {
@@ -707,7 +707,7 @@ class Config:
                 },
                 'det_orientation': {
                     'x': 0,  # deg  Rotation about x axis
-                    'y': 0,  # deg  Rotation about y axis
+                    'y': -15,  # deg  Rotation about y axis
                     'z': 0,  # deg  Rotation about z axis
                 },
                 'hv_channels': {
@@ -739,7 +739,7 @@ class Config:
                 },
                 'det_orientation': {
                     'x': 0,  # deg  Rotation about x axis
-                    'y': 0,  # deg  Rotation about y axis
+                    'y': -15,  # deg  Rotation about y axis
                     'z': 0,  # deg  Rotation about z axis
                 },
                 'hv_channels': {
