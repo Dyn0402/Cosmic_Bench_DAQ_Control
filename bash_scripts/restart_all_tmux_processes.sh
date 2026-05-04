ae364beff09618dd4f42259df1857934f5bbd9ce
Dylan Neff <Dyn0402@hotmail.com>
2026-05-04 21:22:08 +0200

    restart_all_tmux_processes.sh reverted to old path and added debug sleep

--- bash_scripts/restart_all_tmux_processes.sh
+++ bash_scripts/restart_all_tmux_processes.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-START_SERVERS="$SCRIPT_DIR/start_servers.sh"
+START_SERVERS="$SCRIPT_DIR/../start_servers.sh"
 
 # Restart servers in detached screen
 screen -dmS restart_tmux bash -c "
   sleep 2
   $START_SERVERS
+  sleep 20
 "
 # Kill tmux server
 tmux kill-server
--- bash_scripts/restart_daq_tmux_processes.sh
+++ bash_scripts/restart_daq_tmux_processes.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-START_SERVERS="$SCRIPT_DIR/start_servers.sh"
+START_SERVERS="$SCRIPT_DIR/../start_servers.sh"
 
 # Restart servers in detached screen
 screen -dmS restart_tmux bash -c "
